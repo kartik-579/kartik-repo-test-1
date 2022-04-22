9b71472ef81376aa38283836f669242338f319a4
kartik-579 <84493919+kartik-579@users.noreply.github.com>
2022-04-22 13:37:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export const options = {
+  vus: 25,
+  duration: '40s',
+};
+
 export default function () {
   http.get('https://test.k6.io');
   sleep(1);
